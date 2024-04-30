39df894d616e462398b487239749dd36315a20b9
Kush2002 <akhandala352@rku.ac.in>
2024-04-30 13:48:46 +0530

    ok

--- public/js/ranking.js
+++ public/js/ranking.js
@@ -7,12 +7,12 @@ $(document).ready(function() {
         fetchTeam();
     });
 });
-async function fetchSummary() {
+async function fetchTeam() {
     try {
         // let match_id = $('div[data-id]').data('id');
         // // console.log('MatchId:', match_id);
         let formData = new FormData();
-        // formData.append('match_id', match_id);
+        formData.append('type','1');
         let apiUrl = `http://apicricketchampion.in/apiv4/teamRanking/b39d003a77b86b49021b8ba8861bab7c`;
         let response = await fetch(apiUrl, {
             method: 'POST',
@@ -23,9 +23,38 @@ async function fetchSummary() {
         }
         let data = await response.json();
         console.log('API Response:', data);
-        let resHtml = '';
-                resHtml += `<span> done</span>`;
-        $('#btnData').html(resHtml);
+        let resHtml1 = '';
+                resHtml1 += `
+                <div id="com" class="tabcontent" style="display: block;">
+                    <div class="" style="padding:10px">
+                    <div class="rank-grid" style="height:508px;overflow:auto">
+                    <div class="grid-rank" style="">
+                        <h2 class="bg-row-tr" style="padding:10px;margin-bottom:0px">ODI Ranking</h2>
+                        <table style="width:100%;background:#fff">
+                        <tbody>
+                        <tr style="background:#f5f5f5;color:#4539ff">
+                            <th>Rank</th>
+                            <th style="text-align:left">Team</th>
+                            <th>Rating</th>
+                            <th>Points</th>
+                        </tr>`
+                        let resHtml2 ='';
+                        for (let i = 0; i < data.data.length; i++) {
+                        resHtml2 +=
+                            `<tr style="text-align:center;font-size:14px">
+                            <td style="color:#000;">${data.data[i].rank}</td>
+                            <td style="text-align:left">${data.data[i].team}</td>
+                            <td>${data.data[i].rating}</td>
+                            <td>${data.data[i].point}</td>
+                        </tr>
+                        `}`
+                    </tbody>
+                </table>
+                </div>
+                </div>
+                </div>
+                </div>`;
+        $('#btnData').html(resHtml1 + resHtml2 );
         } catch (error) {
         console.error('Error fetching API data:', error);
         $('#btnData').html('<p>Error fetching API data</p>');
